06a735023265c8ce78bf0b886b57aa8dbcf47b8a
toriazao <142436669+toriazao@users.noreply.github.com>
2024-04-30 11:25:29 -0300

    Add files via upload

---
+++ objetos.js
@@ -0,0 +1,15 @@
+class Calebe {
+    constructor (site) {
+        this.site = site; //variável de interface
+        // ações
+    }
+
+    metodo1 (exibirSite) {
+        // ações
+        window.open(this.www.cifraclub.com.br)
+    }
+
+    metodo2 () {
+        // ações
+    }
+}
