cae00796834de3a2339bc53b719e68714f794afa
toriazao <142436669+toriazao@users.noreply.github.com>
2024-04-30 11:45:00 -0300

    Add files via upload

--- objetos.js
+++ objetos.js
@@ -1,15 +1,14 @@
-class Calebe {
+class toria {
     constructor (site) {
         this.site = site; //variável de interface
         // ações
     }
 
-    metodo1 (exibirSite) {
+    exibirSite () {
         // ações
-        window.open(this.www.cifraclub.com.br)
-    }
-
-    metodo2 () {
-        // ações
-    }
+        window.open(this.www.cifraclub.com.br);
+    } 
 }
+
+var novoObjeto = new Objeto ("https://www.cifraclub.com.br/tie/a-noite/");
+novoObjeto.exibirSite();
